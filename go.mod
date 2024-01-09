199befa813bfdc3205455a294ccce4573bc22838
Paul Crane <paul@acheronsoft.com>
2024-01-08 21:59:58 -0600

    Add method Parser.LoadString and associated testing

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Soulsbane/tocparser
 
 go 1.21.5
+
+require github.com/matryer/is v1.4.1 // indirect
--- tocparser/parser.go
+++ tocparser/parser.go
@@ -1,5 +1,7 @@
 package tocparser
 
+import "strings"
+
 // Parser Used for parsing WoW TOC files.
 type Parser struct {
 	values map[string]string
@@ -12,4 +14,38 @@ func New() Parser {
 
 	parser.values = make(map[string]string)
 	return parser
+}
+
+func (parser *Parser) LoadString(content string) bool {
+	if len(content) == 0 {
+		return false
+	}
+
+	lines := strings.Split(string(content), "\n")
+
+	for _, line := range lines {
+		line := strings.TrimSpace(line)
+
+		if strings.HasPrefix(line, "##") {
+			line := strings.Trim(line, "#")
+			values := strings.SplitN(line, ":", 2)
+
+			// Creates a pair from this example string "## Author: Soulsbane"
+			if len(values) == 2 {
+				key := strings.Trim(values[0], " ")
+				value := strings.Trim((values[1]), " ")
+				parser.values[key] = value
+			}
+			// Line is a comment
+		} else if len(line) == 0 || (strings.HasPrefix(line, "#") && !strings.Contains(line, ":")) {
+			continue
+			// Line is a empty or a filename. If blank ignore.
+		} else {
+			if strings.TrimSpace(line) != "" {
+				parser.files = append(parser.files, line)
+			}
+		}
+	}
+
+	return false
 }
